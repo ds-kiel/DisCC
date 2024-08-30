87ad217b2510853b2868afa94899a152d442859e
Julia Rossow <jro@informatik.uni-kiel.de>
2024-08-30 22:32:01 +0200

    Update License

--- auxiliaries/event_log_adjuster.py
+++ auxiliaries/event_log_adjuster.py
@@ -1,3 +1,8 @@
+# Copyright 2024 Kiel University
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from auxiliaries.file_reader import read_event_log
 from datetime import timedelta
 
--- auxiliaries/file_reader.py
+++ auxiliaries/file_reader.py
@@ -1,3 +1,8 @@
+# Copyright 2024 Kiel University
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import pandas as pd
 
 from auxiliaries.csv_reader import read_traces_from_csv
